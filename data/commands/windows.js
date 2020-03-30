9e77c564cf6a7ee2dd00070e9b3a8196e8e476dd
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-03-30 19:54:51 +0200

    created windows help file

---
+++ data/commands/windows.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        "command": " ping",
+        "description": "The ping command is one of the most often used networking utilities for detecting devices on a network and for troubleshooting network problems."
+    },
+    {
+        "command": " ipconfig",
+        "description": "Another indispensable and frequently used utility that is used for finding network information about your local machine like IP addresses, DNS addresses etc"
+    },
+    {
+        "command": " hostname",
+        "description": "A very simple command that displays the host name of your machine."
+    },
+    {
+        "command": "getmac",
+        "description": "Shows the MAC address of your network interfaces"
+    },
+    {
+        "command": "arp",
+        "description": "This is used for showing the address resolution cache. This command must be used with a command line switch arp -a is the most common."
+    },
+    {
+        "command": "nslookup",
+        "description": "Used for checking DNS record entries."
+    },
+    {
+        "command": "netstat",
+        "description": "Used for displaying information about tcp and udp connections and ports."
+    },
+    {
+        "command": "taskKill",
+        "description": "View a list of running tasks using the tasklist command and kill them by name or processor ID using the taskKill command"
+    }
+];
