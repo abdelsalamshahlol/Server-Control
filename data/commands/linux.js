f90f0621de2cd6567a825c1a86f64f194e890ce9
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-03-30 18:59:57 +0200

    changed file format of data to js

---
+++ data/commands/linux.js
@@ -0,0 +1,6 @@
+module.exports = [
+    {
+        "command": "pwd",
+        "description": "pwd stands for Print Work directory and does exactly what you think – it shows the directory you’re currently in. This is one of the handiest Linux terminal commands that aims to make new user’s life peaceful by ensuring they don’t get lost in that seemingly cryptic terminal window."
+    }
+];
